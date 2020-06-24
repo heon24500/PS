ba0c6416b7398b0dbc95fed0d8ccc357bb944dc0
heon24500 <heon24500@naver.com>
2020-06-24 16:03:55 +0900

    Baekjoon Online Judge sources

--- BOJ/14002.cpp
+++ BOJ/14002.cpp
@@ -1,50 +1,45 @@
 #include <iostream>
 using namespace std;
 
-#define N 1005
-int a[N];
-int LIS[N];
-int PATH[N];
-int ret[N];
+const int N = 1001;
+int a[N], d[N], p[N];
+
+void Print(int x) {
+	if (x == -1) return;
+
+	Print(p[x]);
+	cout << a[x] << " ";
+}
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-    int n;
-    cin >> n;
-    for (int i = 1; i <= n; i++) {
-        cin >> a[i];
-    }
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
 
-    for (int i = 1; i <= n; i++) {
-        for (int j = 0; j <= i - 1; j++) {
-            if (a[i] > a[j] && LIS[i] < LIS[j] + 1) {
-                LIS[i] = LIS[j] + 1;
-                PATH[i] = j;
-            }
-        }
-    }
+	int len = 0, ans = 0;
+	for (int i = 0; i < n; i++) {
+		d[i] = 1;
+		p[i] = -1;
+		for (int j = 0; j < i; j++) {
+			if (a[i] > a[j] && d[i] < d[j] + 1) {
+				d[i] = d[j] + 1;
+				p[i] = j;
+			}
+		}
+		
+		if (len < d[i]) {
+			len = d[i];
+			ans = i;
+		}
+	}
 
-    int max = 0, max_num = 0;
-    for (int i = 1; i <= n; i++) {
-        if (max < LIS[i]) {
-            max = LIS[i];
-            max_num = i;
-        }
-    }
-    cout << max << "\n";
+	cout << len << '\n';
+	Print(ans);
 
-    int i = 0;
-    ret[i++] = a[max_num];
-    while (PATH[max_num] > 0) {
-        ret[i++] = a[PATH[max_num]];
-        max_num = PATH[max_num];
-    }
-
-    for (int j = i - 1; j >= 0; j--) {
-        cout << ret[j] << " ";
-    }
-
-    return 0;
+	return 0;
 }
