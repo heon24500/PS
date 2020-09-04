4e72ecd64b542efc1e2980616c68f08adb4f3c5c
heon24500 <heon24500@naver.com>
2020-09-04 13:16:29 +0900

    Baekjoon Online Judge sources

--- BOJ/14888.cpp
+++ BOJ/14888.cpp
@@ -1,65 +1,39 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-typedef pair<long, pair<int, pair<int, pair<int, int> > > > plpipipii;
+const int N = 11;
+int n, max_value = -1e9, min_value = 1e9;
+int mat[N], ops[4];
 
-#define N 15
+void solve(int depth, int num) {
+	if (depth == n) {
+		if (max_value < num) max_value = num;
+		if (min_value > num) min_value = num;
+		return;
+	}
 
-int a[N];
-int calc[4];  // 0 : +,  1 : -,  2 : *,  3 : /
-vector<plpipipii> v[N];
+	for (int i = 0; i < 4; i++) {
+		if (ops[i] == 0) continue;
+		ops[i]--;
+		if (i == 0) solve(depth + 1, num + mat[depth]);
+		else if (i == 1) solve(depth + 1, num - mat[depth]);
+		else if (i == 2) solve(depth + 1, num * mat[depth]);
+		else if (i == 3) solve(depth + 1, num / mat[depth]);
+		ops[i]++;
+	}
+}
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	int n;
 	cin >> n;
 	for (int i = 0; i < n; i++) {
-		cin >> a[i];
+		cin >> mat[i];
 	}
 	for (int i = 0; i < 4; i++) {
-		cin >> calc[i];
+		cin >> ops[i];
 	}
 
-	v[0].push_back({ a[0], {calc[0], {calc[1], {calc[2], calc[3]}}} });
-	for (int i = 1; i < n; i++) {
-		for (int j = 0; j < v[i - 1].size(); j++) {
-			int this_calc[4];
-			long before = v[i - 1][j].first;
-			this_calc[0] = v[i - 1][j].second.first;
-			this_calc[1] = v[i - 1][j].second.second.first;
-			this_calc[2] = v[i - 1][j].second.second.second.first;
-			this_calc[3] = v[i - 1][j].second.second.second.second;
-
-			if (this_calc[0] != 0) {
-				long result = before + a[i];
-				v[i].push_back({ result, {this_calc[0] - 1, {this_calc[1], {this_calc[2], this_calc[3]}}} });
-			}
-			if (this_calc[1] != 0) {
-				long result = before - a[i];
-				v[i].push_back({ result, {this_calc[0], {this_calc[1] - 1, {this_calc[2], this_calc[3]}}} });
-			}
-			if (this_calc[2] != 0) {
-				long result = before * a[i];
-				v[i].push_back({ result, {this_calc[0], {this_calc[1], {this_calc[2] - 1, this_calc[3]}}} });
-			}
-			if (this_calc[3] != 0) {
-				long result = before / a[i];
-				v[i].push_back({ result, {this_calc[0], {this_calc[1], {this_calc[2], this_calc[3] - 1}}} });
-			}
-		}
-	}
-
-	long min = 1000000000, max = -1000000000;
-	for (int i = 0; i < v[n - 1].size(); i++) {
-		long result = v[n - 1][i].first;
-		if (min > result) min = result;
-		if (max < result) max = result;
-	}
-
-	cout << max << "\n" << min;
+	solve(1, mat[0]);
+	cout << max_value << '\n' << min_value << '\n';
 
 	return 0;
 }
