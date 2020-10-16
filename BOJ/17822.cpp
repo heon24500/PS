a63bc41a80634b9264e68c80029e4c7defd3d435
heon24500 <heon24500@naver.com>
2020-10-16 21:42:41 +0900

    원판 돌리기 (구현)

--- BOJ/17822.cpp
+++ BOJ/17822.cpp
@@ -1,100 +1,79 @@
 #include <iostream>
-#include <queue>
 using namespace std;
 
-const int N = 51;
+const int N = 51, M = 51;
 int n, m, t;
-deque<int> dq[N];
+int mat[N][M];
 
 void rotate(int x, int d, int k) {
 	for (int i = x; i <= n; i += x) {
-		if (d == 0) {
-			for (int j = 0; j < k; j++) {
-				int num = dq[i].back();
-				dq[i].pop_back();
-				dq[i].push_front(num);
+		for (int l = 0; l < k; l++) {
+			if (d == 0) {
+				int temp = mat[i][m];
+				for (int j = m; j > 1; j--) mat[i][j] = mat[i][j - 1];
+				mat[i][1] = temp;
 			}
-		}
-		else {
-			for (int j = 0; j < k; j++) {
-				int num = dq[i].front();
-				dq[i].pop_front();
-				dq[i].push_back(num);
+			else {
+				int temp = mat[i][1];
+				for (int j = 1; j < m; j++) mat[i][j] = mat[i][j + 1];
+				mat[i][m] = temp;
 			}
 		}
 	}
 }
 
-int remove() {
-	deque<int> temp[N];
-	for (int i = 1; i <= n; i++) temp[i] = dq[i];
-
-	int cnt = 0;
+void remove() {
+	bool check[N][M] = { 0 };
+	bool isExist = false;
 	for (int i = 1; i <= n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (!dq[i][j]) continue;
+		for (int j = 1; j < m ; j++) {
+			if (mat[i][j] == 0 || mat[i][j] != mat[i][j + 1]) continue;
+			check[i][j] = check[i][j + 1] = true;
+			isExist = true;
+		}
+		if (mat[i][m] == 0 || mat[i][m] != mat[i][1]) continue;
+		check[i][m] = check[i][1] = true;
+		isExist = true;
+	}
 
-			int x = j - 1;
-			if (x < 0) x = m - 1;
-			if (dq[i][j] == dq[i][x]) {
-				temp[i][j] = temp[i][x] = 0;
-				cnt++;
-			}
-			x = j + 1;
-			if (x >= m) x = 0;
-			if (dq[i][j] == dq[i][x]) {
-				temp[i][j] = temp[i][x] = 0;
-				cnt++;
-			}
-
-			int y = i - 1;
-			if (y >= 1 && dq[i][j] == dq[y][j]) {
-				temp[i][j] = temp[y][j] = 0;
-				cnt++;
-			}
-			y = i + 1;
-			if (y <= n && dq[i][j] == dq[y][j]) {
-				temp[i][j] = temp[y][j] = 0;
-				cnt++;
-			}
+	for (int i = 1; i <= m; i++) {
+		for (int j = 1; j < n; j++) {
+			if (mat[j][i] == 0 || mat[j][i] != mat[j + 1][i]) continue;
+			check[j][i] = check[j + 1][i] = true;
+			isExist = true;
 		}
 	}
 
-	for (int i = 1; i <= n; i++) dq[i] = temp[i];
-	return cnt;
-}
-
-int solve(int ret) {
-	int ans = 0;
-	if (ret == 0) {
-		int sum = 0, cnt = 0;
+	if (isExist) {
 		for (int i = 1; i <= n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (!dq[i][j]) continue;
-				sum += dq[i][j];
-				cnt++;
-			}
-		}
-
-		double av = (double)sum / cnt;
-		for (int i = 1; i <= n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (!dq[i][j]) continue;
-				if (dq[i][j] > av) dq[i][j]--;
-				else if (dq[i][j] < av) dq[i][j]++;
-				ans += dq[i][j];
+			for (int j = 1; j <= m; j++) {
+				if (check[i][j]) mat[i][j] = 0;
 			}
 		}
 	}
 	else {
+		int sum = 0, cnt = 0;
 		for (int i = 1; i <= n; i++) {
-			for (int j = 0; j < m; j++) {
-				ans += dq[i][j];
+			for (int j = 1; j <= m; j++) {
+				sum += mat[i][j];
+				if (mat[i][j] != 0) cnt++;
+			}
+		}
+		double avg = (double)sum / cnt;
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				if (mat[i][j] == 0) continue;
+				if (mat[i][j] > avg) mat[i][j]--;
+				else if (mat[i][j] < avg) mat[i][j]++;
 			}
 		}
 	}
+}
 
-	return ans;
+void solve(int x, int d, int k) {
+	rotate(x, d, k);
+	remove();
 }
 
 int main() {
@@ -104,20 +83,22 @@ int main() {
 
 	cin >> n >> m >> t;
 	for (int i = 1; i <= n; i++) {
-		for (int j = 0; j < m; j++) {
-			int num;
-			cin >> num;
-			dq[i].push_back(num);
+		for (int j = 1; j <= m; j++) {
+			cin >> mat[i][j];
 		}
 	}
 
-	int ans = 0;
-	for (int i = 0; i < t; i++) {
+	while (t--) {
 		int x, d, k;
 		cin >> x >> d >> k;
-		rotate(x, d, k);
-		int ret = remove();
-		ans = solve(ret);
+		solve(x, d, k);
+	}
+
+	int ans = 0;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			ans += mat[i][j];
+		}
 	}
 	cout << ans;
 
