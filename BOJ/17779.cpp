86266af59d663c87da142d9f591d55513ca409a5
heon24500 <heon24500@naver.com>
2020-10-17 07:16:19 +0900

    게리맨더링 2 (구현)

--- BOJ/17779.cpp
+++ BOJ/17779.cpp
@@ -2,72 +2,68 @@
 using namespace std;
 
 const int N = 21;
-int n, x, y, d1, d2, ans = 1e9;
+int n, ans = 1e9;
 int a[N][N];
 
-void solve() {
+void solve(int x, int y, int d1, int d2) {
 	int mat[N][N] = { 0 };
+
+	for (int i = 0; i <= d1; i++) {
+		for (int j = i; j <= d1; j++) {
+			mat[x + d1 - i][y - d1 + j] = 5;
+		}
+	}
+	for (int i = 0; i <= d2; i++) {
+		for (int j = 0; j <= i; j++) {
+			mat[x + i][y + j] = 5;
+		}
+	}
+	for (int i = 0; i <= d2; i++) {
+		for (int j = i; j <= d2; j++) {
+			mat[x + d1 + i][y - d1 + j] = 5;
+		}
+	}
+	for (int i = 0; i <= d1; i++) {
+		for (int j = 0; j <= i; j++) {
+			mat[x + d1 + d2 - i][y - d1 + d2 + j] = 5;
+		}
+	}
+
+	for (int i = 1; i < x + d1; i++) {
+		for (int j = 1; j <= y; j++) {
+			if (mat[i][j] == 0) mat[i][j] = 1;
+		}
+	}
+
+	for (int i = 1; i <= x + d2; i++) {
+		for (int j = y + 1; j <= n; j++) {
+			if (mat[i][j] == 0) mat[i][j] = 2;
+		}
+	}
+
+	for (int i = x + d1; i <= n; i++) {
+		for (int j = 1; j < y - d1 + d2; j++) {
+			if (mat[i][j] == 0) mat[i][j] = 3;
+		}
+	}
+
+	for (int i = x + d2; i <= n; i++) {
+		for (int j = y - d1 + d2; j <= n; j++) {
+			if (mat[i][j] == 0) mat[i][j] = 4;
+		}
+	}
+
 	int cnt[6] = { 0 };
-	for (int r = x, c = y; r <= x + d1; r++, c--) mat[r][c] = 5;
-	for (int r = x, c = y; r <= x + d2; r++, c++) mat[r][c] = 5;
-	for (int r = x + d1, c = y - d1; r <= x + d1 + d2; r++, c++) mat[r][c] = 5;
-	for (int r = x + d2, c = y + d2; r <= x + d1 + d2; r++, c--) mat[r][c] = 5;
-	for (int r = x + 1; r < x + d1 + d2; r++) {
-		bool flag = false;
-		for (int c = y - d1; c <= y + d2; c++) {
-			if (flag) {
-				if (mat[r][c] == 0) mat[r][c] = 5;
-				else break;
-			}
-			if (mat[r][c] == 5) flag = true;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cnt[mat[i][j]] += a[i][j];
 		}
 	}
-
-	for (int r = 1; r < x; r++) {
-		for (int c = 1; c <= n; c++) {
-			if (c <= y) mat[r][c] = 1;
-			else mat[r][c] = 2;
-		}
-	}
-	for (int r = x + d1 + d2 + 1; r <= n; r++) {
-		for (int c = 1; c <= n; c++) {
-			if (c < y - d1 + d2) mat[r][c] = 3;
-			else mat[r][c] = 4;
-		}
-	}
-	for (int c = 1; c < y - d1; c++) {
-		for (int r = 1; r <= n; r++) {
-			if (r < x + d1) mat[r][c] = 1;
-			else mat[r][c] = 3;
-		}
-	}
-	for (int c = y + d2 + 1; c <= n; c++) {
-		for (int r = 1; r <= n; r++) {
-			if (r <= x + d2) mat[r][c] = 2;
-			else mat[r][c] = 4;
-		}
-	}
-
-	for (int r = x; r <= x + d1 + d2; r++) {
-		for (int c = y - d1; c <= y + d2; c++) {
-			if (r < x + d1 && c < y && mat[r][c] == 0) mat[r][c] = 1;
-			if (r < x + d2 && c > y && mat[r][c] == 0) mat[r][c] = 2;
-			if (r > x + d1 && c < y - d1 + d2 && mat[r][c] == 0) mat[r][c] = 3;
-			if (r > x + d2 && c > y - d1 + d2 && mat[r][c] == 0) mat[r][c] = 4;
-		}
-	}
-
 	int max_cnt = 0, min_cnt = 1e9;
-	for (int r = 1; r <= n; r++) {
-		for (int c = 1; c <= n; c++) {
-			cnt[mat[r][c]] += a[r][c];
-		}
-	}
 	for (int i = 1; i <= 5; i++) {
 		max_cnt = max(max_cnt, cnt[i]);
 		min_cnt = min(min_cnt, cnt[i]);
 	}
-
 	ans = min(ans, max_cnt - min_cnt);
 }
 
@@ -83,19 +79,17 @@ int main() {
 		}
 	}
 
-	for (x = 1; x <= n; x++) {
-		for (y = 1; y <= n; y++) {
-			for (d1 = 1; d1 <= n; d1++) {
-				for (d2 = 1; d2 <= n; d2++) {
-					if ((x + d1 + d2 > n) || (y - d1 < 1) || (y + d2 > n)) break;
-					solve();
+	for (int x = 1; x < n; x++) {
+		for (int y = 1; y < n; y++) {
+			for (int d1 = 1; d1 < n; d1++) {
+				for (int d2 = 1; d2 < n; d2++) {
+					if (x + d1 + d2 > n || y - d1 < 1 || y + d2 > n) continue;
+					solve(x, y, d1, d2);
 				}
 			}
 		}
 	}
-
 	cout << ans;
-
 
 	return 0;
 }
