e68c5a707e8bdafda2e67e3119df44afba04ef26
heon24500 <heon24500@naver.com>
2020-10-17 08:25:49 +0900

    이차원 배열과 연산 (구현)

--- BOJ/17140.cpp
+++ BOJ/17140.cpp
@@ -5,83 +5,86 @@ using namespace std;
 
 typedef pair<int, int> pii;
 
-const int N = 101;
-int r, c, k, t, rlen = 3, clen = 3;
-int mat[N][N];
+int r, c, k, ans;
+int mat[101][101];
 
-int solve() {
-	while (true) {
-		if (mat[r][c] == k) return t;
-		if (t >= 100) return -1;
-		t++;
-
-		if (rlen >= clen) {
-			int max_len = 0;
-			for (int i = 1; i <= rlen; i++) {
-				vector<pii> v;
-				for (int j = 1; j <= clen; j++) {
-					if (mat[i][j] == 0) continue;
-					int len = v.size();
-					bool flag = false;
-					int l;
-					for (l = 0; l < len; l++) {
-						if (v[l].second == mat[i][j]) {
-							flag = true;
-							break;
-						}
-					}
-					if (flag) v[l].first++;
-					else v.push_back({ 1, mat[i][j] });
+void sortR(int rlen, int clen) {
+	for (int i = 1; i <= rlen; i++) {
+		vector<pii> v;
+		for (int j = 1; j <= clen; j++) {
+			if (mat[i][j] == 0) continue;
+			int len = v.size();
+			if (len == 0) v.push_back({ 1, mat[i][j] });
+			else {
+				bool flag = false;
+				for (int l = 0; l < len; l++) {
+					if (v[l].second != mat[i][j]) continue;
+					v[l].first++;
+					flag = true;
+					break;
 				}
-
-				sort(v.begin(), v.end());
-				int len = v.size();
-				int idx = 1;
-				for (int j = 0; j < len; j++) {
-					mat[i][idx++] = v[j].second;
-					mat[i][idx++] = v[j].first;
-				}
-				max_len = max(max_len, idx - 1);
-				for (int j = idx; j <= clen; j++) {
-					mat[i][j] = 0;
-				}
+				if (!flag) v.push_back({ 1, mat[i][j] });
 			}
-			clen = max_len;
 		}
-		else {
-			int max_len = 0;
-			for (int i = 1; i <= clen; i++) {
-				vector<pii> v;
-				for (int j = 1; j <= rlen; j++) {
-					if (mat[j][i] == 0) continue;
-					int len = v.size();
-					bool flag = false;
-					int l;
-					for (l = 0; l < len; l++) {
-						if (v[l].second == mat[j][i]) {
-							flag = true;
-							break;
-						}
-					}
-					if (flag) v[l].first++;
-					else v.push_back({ 1, mat[j][i] });
-				}
-
-				sort(v.begin(), v.end());
-				int len = v.size();
-				int idx = 1;
-				for (int j = 0; j < len; j++) {
-					mat[idx++][i] = v[j].second;
-					mat[idx++][i] = v[j].first;
-				}
-				max_len = max(max_len, idx - 1);
-				for (int j = idx; j <= rlen; j++) {
-					mat[j][i] = 0;
-				}
-			}
-			rlen = max_len;
+		sort(v.begin(), v.end());
+		int idx = 1, len = v.size();
+		for (int l = 0; l < len; l++) {
+			mat[i][idx++] = v[l].second;
+			mat[i][idx++] = v[l].first;
 		}
+		while (idx <= 100) mat[i][idx++] = 0;
 	}
+}
+
+void sortC(int rlen, int clen) {
+	for (int j = 1; j <= clen; j++) {
+		vector<pii> v;
+		for (int i = 1; i <= rlen; i++) {
+			if (mat[i][j] == 0) continue;
+			int len = v.size();
+			if (len == 0) v.push_back({ 1, mat[i][j] });
+			else {
+				bool flag = false;
+				for (int l = 0; l < len; l++) {
+					if (v[l].second != mat[i][j]) continue;
+					v[l].first++;
+					flag = true;
+					break;
+				}
+				if (!flag) v.push_back({ 1, mat[i][j] });
+			}
+		}
+		sort(v.begin(), v.end());
+		int idx = 1, len = v.size();
+		for (int l = 0; l < len; l++) {
+			mat[idx++][j] = v[l].second;
+			mat[idx++][j] = v[l].first;
+		}
+		while (idx <= 100) mat[idx++][j] = 0;
+	}
+}
+
+void solve() {
+	while (ans <= 100) {
+		if (mat[r][c] == k) return;
+
+		int rlen = 0, clen = 0;
+		for (int i = 1; mat[i][1]; i++) {
+			for (int j = 1; mat[i][j]; j++) {
+				if (clen < j) clen = j;
+			}
+		}
+		for (int j = 1; mat[1][j]; j++) {
+			for (int i = 1; mat[i][j]; i++) {
+				if (rlen < i) rlen = i;
+			}
+		}
+
+		if (rlen >= clen) sortR(rlen, clen);
+		else sortC(rlen, clen);
+		ans++;
+	}
+	ans = -1;
 }
 
 int main() {
@@ -95,7 +98,9 @@ int main() {
 			cin >> mat[i][j];
 		}
 	}
-	cout << solve();
+
+	solve();
+	cout << ans;
 
 	return 0;
 }
