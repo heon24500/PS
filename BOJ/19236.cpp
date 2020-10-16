b6bb873870c9a3c6009b0e153af348ef3ea01756
heon24500 <heon24500@naver.com>
2020-10-16 09:16:14 +0900

    청소년 상어 (구현, 백트래킹)

--- BOJ/19236.cpp
+++ BOJ/19236.cpp
@@ -2,110 +2,97 @@
 using namespace std;
 
 struct Fish {
-	int x;
-	int y;
-	int d;
+	int x, y, d;
 	bool alive;
 };
 
-Fish fish[20];
+int sx, sy, sd, ans;
 int mat[4][4];
-int ans;
 int dx[9] = { 0, -1, -1, 0, 1, 1, 1, 0, -1 };
 int dy[9] = { 0, 0, -1, -1, -1, 0, 1, 1, 1 };
+Fish fish[17];
 
-void backup(Fish fish_bak[20], int mat_bak[4][4], bool backup) {
-	if (backup) {
-		for (int i = 0; i < 20; i++) fish_bak[i] = fish[i];
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 4; j++) {
-				mat_bak[i][j] = mat[i][j];
-			}
-		}
-	}
-	else {
-		for (int i = 0; i < 20; i++) fish[i] = fish_bak[i];
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 4; j++) {
-				mat[i][j] = mat_bak[i][j];
-			}
-		}
-	}
-}
-
-bool isPossible(int tx, int ty) {
-	if (ty < 0 || ty >= 4 || tx < 0 || tx >= 4) return false;
+bool isPossible(int x, int y) {
+	if (x < 0 || x > 3 || y < 0 || y > 3 || mat[x][y] == -1) return false;
 	return true;
-}
-
-void moveShark(int x, int y, int tx, int ty, int fish_num, bool move) {
-	if (move) {
-		mat[x][y] = 0;
-		mat[tx][ty] = -1;
-		fish[fish_num].alive = false;
-	}
-	else {
-		mat[x][y] = -1;
-		mat[tx][ty] = fish_num;
-		fish[fish_num].alive = true;
-	}
 }
 
 void moveFish() {
 	for (int i = 1; i <= 16; i++) {
 		if (!fish[i].alive) continue;
-
 		int x = fish[i].x;
 		int y = fish[i].y;
 		int d = fish[i].d;
 
-		for (int j = 0; j < 9; j++) {
-			int td = d + j;
-			if (td > 8) td -= 8;
-			int tx = x + dx[td];
-			int ty = y + dy[td];
-			int target = mat[tx][ty];
+		for (int j = 1; j <= 8; j++) {
+			int tx = x + dx[d];
+			int ty = y + dy[d];
+			if (!isPossible(tx, ty)) {
+				d++;
+				if (d > 8) d -= 8;
+				fish[i].d = d;
+				continue;
+			}
 
-			if (!isPossible(tx, ty) || target == -1) continue;
-
+			swap(mat[x][y], mat[tx][ty]);
+			int num = mat[x][y];
 			fish[i].x = tx;
 			fish[i].y = ty;
-			fish[i].d = td;
-			fish[target].x = x;
-			fish[target].y = y;
-			mat[x][y] = target;
-			mat[tx][ty] = i;
-
+			fish[num].x = x;
+			fish[num].y = y;
 			break;
 		}
 	}
 }
 
-// DFS를 활용한 Backtracking. 상어의 위치 x, y와 방향 d, 먹은 물고기 총합 sum
-void solve(int x, int y, int dir, int sum) {
+void backup(Fish fish_bak[17], int mat_bak[4][4], bool isBackup) {
+	if (isBackup) {
+		fish_bak[0] = { sx, sy, sd, true };
+		for (int i = 1; i <= 16; i++) fish_bak[i] = fish[i];
+		for (int x = 0; x < 4; x++) {
+			for (int y = 0; y < 4; y++) {
+				mat_bak[x][y] = mat[x][y];
+			}
+		}
+	}
+	else {
+		sx = fish_bak[0].x;
+		sy = fish_bak[0].y;
+		sd = fish_bak[0].d;
+		for (int i = 1; i <= 16; i++) fish[i] = fish_bak[i];
+		for (int x = 0; x < 4; x++) {
+			for (int y = 0; y < 4; y++) {
+				mat[x][y] = mat_bak[x][y];
+			}
+		}
+	}
+}
+
+void solve(int sum) {
 	ans = max(ans, sum);
 
-	// Backtracking을 위해 mat과 fish를 저장함
-	Fish fish_bak[20];
+	Fish fish_bak[17];
 	int mat_bak[4][4];
+
+	moveFish();
 	backup(fish_bak, mat_bak, true);
 
-	// 작업
-	moveFish();
-
 	for (int i = 1; i <= 3; i++) {
-		int tx = x + dx[dir] * i;
-		int ty = y + dy[dir] * i;
+		int tx = sx + dx[sd] * i;
+		int ty = sy + dy[sd] * i;
 		if (!isPossible(tx, ty) || mat[tx][ty] == 0) continue;
 
-		int fish_num = mat[tx][ty];
-		int td = fish[fish_num].d;
-		moveShark(x, y, tx, ty, fish_num, true);
-		solve(tx, ty, td, sum + fish_num);
-		moveShark(x, y, tx, ty, fish_num, false);
-	}
+		int num = mat[tx][ty];
+		fish[num].alive = false;
+		mat[tx][ty] = -1;
+		mat[sx][sy] = 0;
+		sd = fish[num].d;
+		sx = tx;
+		sy = ty;
 
-	backup(fish_bak, mat_bak, false);
+		solve(sum + num);
+		backup(fish_bak, mat_bak, false);
+	}
 }
 
 int main() {
@@ -113,18 +100,21 @@ int main() {
 	cin.tie(0);
 	cout.tie(0);
 
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			int num, dir;
-			cin >> num >> dir;
-			mat[i][j] = num;
-			fish[num] = { i, j, dir, true };
+	for (int x = 0; x < 4; x++) {
+		for (int y = 0; y < 4; y++) {
+			int n, d;
+			cin >> n >> d;
+			mat[x][y] = n;
+			fish[n] = { x, y, d, true };
 		}
 	}
+	int num = mat[sx][sy];
+	mat[sx][sy] = -1;
+	sd = fish[num].d;
+	fish[num].alive = false;
 
-	int target = mat[0][0];
-	fish[target].alive = false;
-	mat[0][0] = -1;
-	solve(0, 0, fish[target].d, target);
+	solve(num);
 	cout << ans;
+
+	return 0;
 }
