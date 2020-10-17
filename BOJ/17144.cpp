433f5c93bf51e6f78f36d6524eb03739cc02d277
heon24500 <heon24500@naver.com>
2020-10-17 09:45:51 +0900

    미세먼지 안녕! (구현)

--- BOJ/17144.cpp
+++ BOJ/17144.cpp
@@ -1,83 +1,104 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-typedef vector<vector<int> > vii;
+typedef pair<int, int> pii;
 
-int r, c, t, r1, r2;
+const int R = 51, C = 51;
+int r, c, t;
+int mat[R][C];
 int dy[4] = { 0, 1, 0, -1 };
 int dx[4] = { 1, 0, -1, 0 };
-vii mat;
+pii machine;
 
-bool isPossible(int ty, int tx) {
-	if (ty < 0 || ty >= r || tx < 0 || tx >= c || mat[ty][tx] == -1) return false;
+bool isPossible(int y, int x) {
+	if (y < 0 || y >= r || x < 0 || x >= c || mat[y][x] == -1) return false;
 	return true;
 }
 
-void solve() {
-	int times = 0;
-	while (true) {
-		// 확산
-		vii ret = mat;
-		for (int i = 0; i < r; i++) {
-			for (int j = 0; j < c; j++) {
-				if (mat[i][j] <= 0) continue;
+void spread() {
+	int next_mat[R][C] = { 0 };
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			if (mat[i][j] == -1) next_mat[i][j] = -1;
+			if (mat[i][j] <= 0) continue;
 
-				int cnt = 0;
-				for (int k = 0; k < 4; k++) {
-					int ti = i + dy[k];
-					int tj = j + dx[k];
-					if (!isPossible(ti, tj)) continue;
-
-					ret[ti][tj] += mat[i][j] / 5;
-					cnt++;
-				}
-				ret[i][j] -= (mat[i][j] / 5) * cnt;
-			}
-		}
-		mat = ret;
-
-		// 공기청정기 작동
-		for (int i = r1 - 2; i >= 0; i--) mat[i + 1][0] = mat[i][0];
-		for (int i = 1; i < c; i++) mat[0][i - 1] = mat[0][i];
-		for (int i = 1; i <= r1; i++) mat[i - 1][c - 1] = mat[i][c - 1];
-		for (int i = c - 2; i >= 1; i--) mat[r1][i + 1] = mat[r1][i];
-		mat[r1][1] = 0;
-
-		for (int i = r2 + 2; i < r; i++) mat[i - 1][0] = mat[i][0];
-		for (int i = 1; i < c; i++) mat[r - 1][i - 1] = mat[r - 1][i];
-		for (int i = r - 2; i >= r2; i--) mat[i + 1][c - 1] = mat[i][c - 1];
-		for (int i = c - 2; i >= 1; i--) mat[r2][i + 1] = mat[r2][i];
-		mat[r2][1] = 0;
-
-		times++;
-		if (times == t) {
 			int cnt = 0;
-			for (int i = 0; i < r; i++) {
-				for (int j = 0; j < c; j++) {
-					if (mat[i][j] > 0) cnt += mat[i][j];
-				}
+			for (int k = 0; k < 4; k++) {
+				int ty = i + dy[k];
+				int tx = j + dx[k];
+				if (!isPossible(ty, tx)) continue;
+				next_mat[ty][tx] += mat[i][j] / 5;
+				cnt++;
 			}
-			cout << cnt;
-			return;
+
+			next_mat[i][j] += mat[i][j] - (mat[i][j] / 5) * cnt;
 		}
+	}
+
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			mat[i][j] = next_mat[i][j];
+		}
+	}
+}
+
+void run() {
+	int y = machine.first;
+	int x = machine.second;
+
+	int idx = y - 2;
+	while (idx >= 0) {
+		mat[idx + 1][x] = mat[idx][x];
+		idx--;
+	}
+	for (int i = 0; i < c - 1; i++) mat[0][i] = mat[0][i + 1];
+	for (int i = 0; i < y; i++) mat[i][c - 1] = mat[i + 1][c - 1];
+	for (int i = c - 1; i > 0; i--) mat[y][i] = mat[y][i - 1];
+	mat[y][x + 1] = 0;
+
+	idx = y + 3;
+	while (idx < r) {
+		mat[idx - 1][x] = mat[idx][x];
+		idx++;
+	}
+	for (int i = 0; i < C - 1; i++) mat[r - 1][i] = mat[r - 1][i + 1];
+	for (int i = r - 1; i > y + 1; i--) mat[i][c - 1] = mat[i - 1][c - 1];
+	for (int i = c - 1; i > 0; i--) mat[y + 1][i] = mat[y + 1][i - 1];
+	mat[y + 1][x + 1] = 0;
+}
+
+void solve() {
+	while (t--) {
+		spread();
+		run();
 	}
 }
 
 int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
 	cin >> r >> c >> t;
 	for (int i = 0; i < r; i++) {
-		vector<int> temp(c);
 		for (int j = 0; j < c; j++) {
-			cin >> temp[j];
-			if (temp[j] == -1) {
-				if (r1 == 0) r1 = i;
-				else r2 = i;
+			cin >> mat[i][j];
+			if (mat[i][j] == -1 && machine.first == 0) {
+				machine.first = i;
+				machine.second = j;
 			}
 		}
-		mat.push_back(temp);
 	}
+
 	solve();
+	int ans = 0;
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			if (mat[i][j] <= 0) continue;
+			ans += mat[i][j];
+		}
+	}
+	cout << ans;
 
 	return 0;
 }
