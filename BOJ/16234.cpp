f878cf2de0da733b5d772d0d13e35f3507d6a360
heon24500 <heon24500@naver.com>
2020-10-17 15:02:49 +0900

    인구 이동 (구현, BFS)

--- BOJ/16234.cpp
+++ BOJ/16234.cpp
@@ -1,87 +1,91 @@
 #include <iostream>
-#include <vector>
 #include <queue>
 using namespace std;
 
 typedef pair<int, int> pii;
-const int N = 50;
-int n, l, r;
+
+const int N = 51;
+int n, l, r, ans;
 int a[N][N];
 int dy[4] = { 0, 1, 0, -1 };
 int dx[4] = { 1, 0, -1, 0 };
-bool visited[N][N];
 
 bool isPossible(int y, int x, int ty, int tx) {
-	if (ty < 0 || ty >= n || tx < 0 || tx >= n || visited[ty][tx]) return false;
-	int diff = abs(a[ty][tx] - a[y][x]);
-	if (diff < l || diff > r) return false;
+	if (ty < 0 || ty >= n || tx < 0 || tx >= n || abs(a[y][x] - a[ty][tx]) < l || abs(a[y][x] - a[ty][tx]) > r) return false;
 	return true;
 }
 
-int bfs(pii n) {
-	int ret = 0;
-	queue<pii> q;
-	q.push(n);
-	visited[n.first][n.second] = true;
+void solve() {
+	while (true) {
+		queue<pii> q;
+		int town = 1;
+		int mat[N][N] = { 0 };
+		bool visited[N][N] = { 0 };
 
-	vector<pii> u;
-	while (!q.empty()) {
-		pii now = q.front();
-		q.pop();
+		for (int y = 0; y < n; y++) {
+			for (int x = 0; x < n; x++) {
+				if (visited[y][x]) continue;
 
-		u.push_back(now);
+				q.push({ y, x });
+				visited[y][x] = true;
 
-		int y = now.first;
-		int x = now.second;
+				while (!q.empty()) {
+					pii now = q.front();
+					q.pop();
 
-		for (int i = 0; i < 4; i++) {
-			int ty = y + dy[i];
-			int tx = x + dx[i];
-			if (!isPossible(y, x, ty, tx)) continue;
-			q.push(make_pair(ty, tx));
-			visited[ty][tx] = true;
+					int ny = now.first;
+					int nx = now.second;
+					mat[ny][nx] = town;
+
+					for (int i = 0; i < 4; i++) {
+						int ty = ny + dy[i];
+						int tx = nx + dx[i];
+						if (!isPossible(ny, nx, ty, tx) || visited[ty][tx]) continue;
+						q.push({ ty, tx });
+						visited[ty][tx] = true;
+					}
+				}
+
+				town++;
+			}
 		}
+
+		int cnt[N * N] = { 0 }, sum[N * N] = { 0 };
+		for (int y = 0; y < n; y++) {
+			for (int x = 0; x < n; x++) {
+				cnt[mat[y][x]]++;
+				sum[mat[y][x]] += a[y][x];
+			}
+		}
+
+		bool flag = false;
+		for (int y = 0; y < n; y++) {
+			for (int x = 0; x < n; x++) {
+				if (cnt[mat[y][x]] == 1) continue;
+
+				a[y][x] = sum[mat[y][x]] / cnt[mat[y][x]];
+				flag = true;
+			}
+		}
+
+		if (flag) ans++;
+		else break;
 	}
-	if (u.size() > 1) {
-		ret++;
-		int sum = 0;
-		for (int i = 0; i < u.size(); i++) {
-			sum += a[u[i].first][u[i].second];
-		}
-		sum /= u.size();
-		for (int i = 0; i < u.size(); i++) {
-			a[u[i].first][u[i].second] = sum;
-		}
-	}
-	return ret;
 }
 
 int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
 	cin >> n >> l >> r;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			cin >> a[i][j];
 		}
 	}
-
-	int ret = 0;
-	while (true) {
-		int cnt = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (visited[i][j]) continue;
-				cnt += bfs(make_pair(i, j));
-			}
-		}
-		if (cnt == 0) break;
-		ret++;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				visited[i][j] = false;
-			}
-		}
-	}
-	cout << ret;
+	solve();
+	cout << ans;
 
 	return 0;
 }
