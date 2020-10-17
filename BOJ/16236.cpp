192621f932f94e2da22fe988644189b8785cc220
heon24500 <heon24500@naver.com>
2020-10-17 15:53:17 +0900

    아기 상어 (구현, BFS)

--- BOJ/16236.cpp
+++ BOJ/16236.cpp
@@ -3,86 +3,87 @@
 using namespace std;
 
 typedef pair<int, int> pii;
-typedef pair<int, pii> pipii;
 
-const int N = 20;
-int n, y, x, ans, cnt, shark = 2;
+struct Shark {
+	int y, x, size = 2, eat;
+};
+
+const int N = 21;
+int n, ans;
 int mat[N][N];
 int dy[4] = { 0, 1, 0, -1 };
 int dx[4] = { 1, 0, -1, 0 };
+Shark shark;
 
-bool isPossible(int ty, int tx) {
-	if (ty < 0 || ty >= n || tx < 0 || tx >= n || mat[ty][tx] > shark) return false;
+bool isPossible(int y, int x) {
+	if (y < 0 || y >= n || x < 0 || x >= n || mat[y][x] > shark.size) return false;
 	return true;
-}
-
-pipii bfs() {
-	queue<pii> q;
-	q.push({ y, x });
-	bool visited[N][N] = { 0 };
-	int dist[N][N] = { 0 };
-	visited[y][x] = true;
-
-	pii ret = { -1, -1 };
-	int min_dist = 1e9;
-	while (!q.empty()) {
-		pii now = q.front();
-		q.pop();
-
-		if (!(now.first == y && now.second == x) && mat[now.first][now.second] != 0 && mat[now.first][now.second] < shark) {
-			if (min_dist > dist[now.first][now.second]) {
-				ret = { now.first, now.second };
-				min_dist = dist[now.first][now.second];
-			}
-			else if (min_dist == dist[now.first][now.second]) {
-				if ((ret.first > now.first) || ((ret.first == now.first) && (ret.second > now.second))) ret = { now.first, now.second };
-			}
-		}
-
-		for (int i = 0; i < 4; i++) {
-			int ty = now.first + dy[i];
-			int tx = now.second + dx[i];
-			if (!isPossible(ty, tx)) continue;
-			if (visited[ty][tx]) continue;
-			q.push({ ty, tx });
-			visited[ty][tx] = true;
-			dist[ty][tx] = dist[now.first][now.second] + 1;
-		}
-	}
-
-	return { min_dist, ret };
 }
 
 void solve() {
 	while (true) {
-		pipii ret = bfs();
-		int dist = ret.first;
-		pii fish = ret.second;
-		if (fish.first == -1 && fish.second == -1) return;
-		mat[y][x] = 0;
-		y = fish.first;
-		x = fish.second;
-		mat[y][x] = 9;
-		cnt++;
-		if (cnt == shark) {
-			shark++;
-			cnt = 0;
+		bool visited[N][N] = { 0 };
+		int dist[N][N] = { 0 };
+		queue<pii> q;
+		q.push({ shark.y, shark.x });
+		visited[shark.y][shark.x] = true;
+
+		while (!q.empty()) {
+			pii now = q.front();
+			q.pop();
+			int y = now.first;
+			int x = now.second;
+			for (int i = 0; i < 4; i++) {
+				int ty = y + dy[i];
+				int tx = x + dx[i];
+				if (!isPossible(ty, tx) || visited[ty][tx]) continue;
+				q.push({ ty, tx });
+				visited[ty][tx] = true;
+				dist[ty][tx] = dist[y][x] + 1;
+			}
 		}
-		ans += dist;
+
+		int min_dist = 1e9, dest_y = 0, dest_x = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (!visited[i][j] || mat[i][j] == 0 || mat[i][j] >= shark.size || min_dist <= dist[i][j]) continue;
+				min_dist = dist[i][j];
+				dest_y = i;
+				dest_x = j;
+			}
+		}
+
+		if (min_dist == 1e9) break;
+		ans += min_dist;
+
+		mat[dest_y][dest_x] = 9;
+		mat[shark.y][shark.x] = 0;
+		shark.y = dest_y;
+		shark.x = dest_x;
+		shark.eat++;
+		if (shark.eat == shark.size) {
+			shark.size++;
+			shark.eat = 0;
+		}
 	}
 }
 
 int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			cin >> mat[i][j];
 			if (mat[i][j] == 9) {
-				y = i;
-				x = j;
+				shark.y = i;
+				shark.x = j;
 			}
 		}
 	}
+
 	solve();
 	cout << ans;
 
