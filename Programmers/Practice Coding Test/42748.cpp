13cbec4111567861bb9654cf77d907565f5043e2
heon24500 <heon24500@naver.com>
2020-10-23 19:40:18 +0900

    K번째 수

--- Programmers/Practice Coding Test/42748.cpp
+++ Programmers/Practice Coding Test/42748.cpp
@@ -1,23 +1,21 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
 
-    for (int cnt = 0; cnt < commands.size(); cnt++) {
-        int i = commands[cnt][0];
-        int j = commands[cnt][1];
-        int k = commands[cnt][2];
+    for (int i = 0; i < commands.size(); i++) {
+        int a = commands[i][0] - 1;
+        int b = commands[i][1] - 1;
+        int k = commands[i][2] - 1;
 
-        vector<int> temp;
-        for (int t = i - 1; t < j; t++) {
-            temp.push_back(array[t]);
-        }
-        sort(temp.begin(), temp.end());
-        answer.push_back(temp[k - 1]);
+        vector<int> v;
+        for (int j = a; j <= b; j++) v.push_back(array[j]);
+        sort(v.begin(), v.end());
+        answer.push_back(v[k]);
     }
-
     return answer;
 }
