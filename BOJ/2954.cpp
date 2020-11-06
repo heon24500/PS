adbf8a497cbc9cc08d0807e21347eef64c692706
heon24500 <heon24500@naver.com>
2020-11-06 13:18:55 +0900

    창영이의 일기장

---
+++ BOJ/2954.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	string s;
+	getline(cin, s);
+
+	bool flag = false;
+	for (int i = 0; s[i]; i++) {
+		if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+			if (flag) {
+				flag = false;
+				continue;
+			}
+			else {
+				cout << s[i];
+				flag = true;
+			}
+		}
+		else if (flag) continue;
+		else cout << s[i];
+	}
+
+	return 0;
+}
