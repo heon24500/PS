9c62dbebad5fd87c0159c04db396ee9437bd29c0
heon24500 <heon24500@naver.com>
2020-12-09 12:16:27 +0900

    문자사각형1

---
+++ Jongol/1307.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+char arr[100][100];
+char ch = 'A';
+int n;
+
+void fill() {
+	for (int i = n - 1; i >= 0; i--) {
+		for (int j = n - 1; j >= 0; j--) {
+			arr[j][i] = ch++;
+			if (ch > 'Z') ch = 'A';
+		}
+	}
+}
+
+void print() {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << '\n';
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	fill();
+	print();
+
+	return 0;
+}
