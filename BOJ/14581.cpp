6d4f1a37b3b6b84daf0bbfeed4540382757b8333
heon24500 <heon24500@naver.com>
2020-12-14 13:24:37 +0900

    팬들에게 둘러싸인 홍준

---
+++ BOJ/14581.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	string str;
+	cin >> str;
+	cout << ":fan::fan::fan:\n";
+	cout << ":fan::" + str + "::fan:\n";
+	cout << ":fan::fan::fan:\n";
+
+	return 0;
+}
