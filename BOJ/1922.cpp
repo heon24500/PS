025e1e6211b9bed232c407f285728a3b467a4171
heon24500 <heon24500@naver.com>
2020-12-26 22:01:35 +0900

    네트워크 연결

--- BOJ/1922.cpp
+++ BOJ/1922.cpp
@@ -38,6 +38,9 @@ bool findParent(int a, int b) {
 }
 
 int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
 	cin >> n >> m;
 	for (int i = 1; i <= n; i++) parent[i] = i;
 	for (int i = 0; i < m; i++) {
