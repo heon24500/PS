85e5b744950672d78a26444906a48de113681b23
heon24500 <heon24500@naver.com>
2021-01-17 14:06:18 +0900

    이진수 합

---
+++ BOJ/8741.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int k;
+	cin >> k;
+	for (int i = 0; i < k; i++) cout << "1";
+	for (int i = 1; i < k; i++) cout << "0";
+
+	return 0;
+}
